3ca073e28db6eddc68699ac1428f7f0e1630fd1f
Alex <44152140+alx2002@users.noreply.github.com>
2018-11-07 21:35:27 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -4,11 +4,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
-using System.Collections;
+//using System.Collections;
 
 namespace DuplicateFileFinder
 {
-    public class FileNameandHash
+    public class organizer
     {
         public string FileName { get; set; }
         public string FileHash { get; set; }
@@ -16,155 +16,159 @@ namespace DuplicateFileFinder
 }
 
 
-    
 
-
-namespace DuplicateFileFinder
-{
-    public class Program
+    namespace DuplicateFileFinder
     {
-
-        static void Main(string[] args)
+        public class Program
         {
-            string path;
-            ConsoleKeyInfo cki;
-            //identifies the console key that was pressed.
-            double totalSize = 0;
 
-
-            //pass directory path as argument to command line
-
-            if (args.Length > 0)
-                path = args[0] as string;
-            else
-                path = @"C:\Users\Alex\Desktop\Test11";
-            //if this is wrong will throw exception
-
-            
-            Console.Clear();
-
-            if (!Directory.Exists(path))
+            static void
+                Main(string[] args)
             {
-                Console.WriteLine("{0} is not a valid file or directory", path);
-            }
+                string path;
+                ConsoleKeyInfo cki;
+                //identifies the console key that was pressed.
+                decimal totalSize = 0;
+                //double
 
+               
+                //pass directory path as argument to command line
 
-            if (Directory.Exists(path))
-            {
-                Console.WriteLine("Directory Location: {0} ", path);
-
-            }
-            else if (Directory.Exists(path))
-            {
-                Console.WriteLine("{0} is not a valid file or directory.", path);
-            }
-
-
-
-        
-
-        var fileLists = Directory.GetFiles(path);
-            // will send you here if path is invalid.
-            
+                if (args.Length > 0)
+                    path = args[0] as string;
+                else
+                    path = @"C:\Users\Alex\Desktop\Test1"; //@"C:\Users\Alex\Desktop\Test11";
+                                                           //if this is not found on your system throw exception
+                                                           //folder location
 
 
 
-            int totalFiles = fileLists.Length;
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("Files detected:{0} \n", totalFiles);
-            Console.ForegroundColor = ConsoleColor.White;
+                Console.Clear();
 
-
-
-            List<FileNameandHash> info = new List<FileNameandHash>();
-            List<string> ToDelete = new List<string>();
-            info.Clear();
-            //loop through all the files by file hash code
-            foreach (var item in fileLists)
-            {
-                using (var ReadStream = new FileStream(item, FileMode.Open, FileAccess.Read))
-                // open stream and read
+                if (!Directory.Exists(path))
                 {
-                    info.Add(new FileNameandHash()
-                    {
-                        FileName = item,
-                        FileHash = BitConverter.ToString(MD5.Create().ComputeHash(ReadStream)),
-                        //MD5 for speed
-                    });
+                    Console.WriteLine("{0} is not a valid file or directory", path);
                 }
-            }
-            //group by file hash code
-            var SList = info.GroupBy(f => f.FileHash)
-                .Select(g => new { FileHash = g.Key, Files = g.Select(z => z.FileName).ToList() });
 
 
-            //keeping first item of each group as is and identify rest as duplicate files to delete
-            ToDelete.AddRange(SList.SelectMany(f => f.Files.Skip(1)).ToList());
-            Console.WriteLine("Duplicates detected:{0}\n", ToDelete.Count);
-            Console.ForegroundColor = ConsoleColor.White;
-
-            //list all files to be deleted and count total disk space to be empty after delete
-            //chooses one of the files to keep, so if there is 3 (A,B,C) duplicates it keeps B and deletes A and C
-            if (ToDelete.Count > 0)
-            {
-                Console.WriteLine("Files to be deleted:");
-                foreach (var item in ToDelete)
+                if (Directory.Exists(path))
                 {
-                    Console.WriteLine(item);
-                    FileInfo fi = new FileInfo(item);
-                    totalSize += fi.Length;
+                    Console.WriteLine("Directory Location: {0} ", path);
+
                 }
-            }
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            Console.WriteLine("\nWill free: {0}  MEGABYTE(S)\n", Math.Round((totalSize / 10000), 4).ToString());
-            Console.ForegroundColor = ConsoleColor.White;
-            //delete duplicate files
-            if (0 < ToDelete.Count)
-            // Do this if count if greater than 0
-            {
-                Console.WriteLine("Press I to initilize deletion.");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Press the Q key to quit. \n");
-                Console.ForegroundColor = ConsoleColor.White;
-                do
-
+                else if (Directory.Exists(path))
                 {
-                    Console.TreatControlCAsInput = true;
+                    Console.WriteLine("{0} is not a valid file or directory.", path);
+                }
 
-                    cki = Console.ReadKey();
-                    /*if((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
-                    if((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
-                    if((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+"); 
-                     */
-                    Console.WriteLine(cki.Key.ToString());
-                    Console.ReadKey();
 
-                    Console.WriteLine(" You pressed {0}\n", cki.Key.ToString());
 
-                    Console.ForegroundColor = ConsoleColor.White;
-                    if (cki.Key == ConsoleKey.I)
+
+
+
+
+                var fileLists = Directory.GetFiles(path);
+                // will send you here if path is invalid.
+
+
+
+
+                int totalFiles = fileLists.Length;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Files detected:{0} \n", totalFiles);
+                Console.ForegroundColor = ConsoleColor.White;
+
+
+
+                List<organizer> info = new List<organizer>();
+                List<string> ToDelete = new List<string>();
+                info.Clear();
+                //loop through all the files by file hash code
+                foreach (var item in fileLists)
+                {
+                    using (var ReadStream = new FileStream(item, FileMode.Open, FileAccess.Read))
+                    // open stream and read
                     {
-                        Console.WriteLine("Deleting files... \n");
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        ToDelete.ForEach(File.Delete);
-                        Console.WriteLine("Files are deleted successfully. \n");
-                        Console.ForegroundColor = ConsoleColor.Green;
+                        info.Add(new organizer()
+                        {
+                            FileName = item,
+                            FileHash = BitConverter.ToString(MD5.Create().ComputeHash(ReadStream)),
+                            //Hash MD5 for each file
+                        });
                     }
-                    Console.WriteLine("Press Q key to quit.\n");
-                } while (cki.Key != ConsoleKey.Q);
+                }
+                //group  by file hash code
+                var SList = info.GroupBy(f => f.FileHash)
+                    .Select(g => new { FileHash = g.Key, Files = g.Select(z => z.FileName).ToList() });
+
+
+                //keeping first item of each group as is and identify rest as duplicate files usedb to delete
+                ToDelete.AddRange(SList.SelectMany(f => f.Files.Skip(1)).ToList());
+                Console.WriteLine("Duplicates detected:{0}\n", ToDelete.Count);
+                Console.ForegroundColor = ConsoleColor.White;
+
+                //list all files to be deleted and count total disk space to be empty after delete
+                //chooses one of the files to keep, so if there is 3 (A,B,C) duplicates it keeps B and deletes A and C
+                if (ToDelete.Count > 0)
+                {
+                    Console.WriteLine("Files to be deleted:");
+                    foreach (var item in ToDelete)
+                    {
+                        Console.WriteLine(item);
+                        FileInfo fi = new FileInfo(item);
+                        totalSize += fi.Length;
+                    }
+                }
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine("\nWill free: {0}  MEGABYTE(S)\n", Math.Round((totalSize / 10000), 4).ToString());
+                Console.ForegroundColor = ConsoleColor.White;
+                //delete duplicate files
+                if (0 < ToDelete.Count)
+                // Do this if count if greater than 0
+                {
+                    Console.WriteLine("Press I to initilize deletion.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("Press the Q key to quit. \n");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    do
+
+                    {
+                        Console.TreatControlCAsInput = true;
+                        //Provides info about key pressed
+                        cki = Console.ReadKey();
+                        /*if((cki.Modifiers & ConsoleModifiers.Alt) != 0) Console.Write("ALT+");
+                        if((cki.Modifiers & ConsoleModifiers.Shift) != 0) Console.Write("SHIFT+");
+                        if((cki.Modifiers & ConsoleModifiers.Control) != 0) Console.Write("CTL+"); 
+                         */ // no use for these
+                        Console.WriteLine(cki.Key.ToString());
+                        Console.ReadKey();
+
+                        Console.WriteLine(" You pressed {0}\n", cki.Key.ToString());
+
+                        Console.ForegroundColor = ConsoleColor.White;
+                        if (cki.Key == ConsoleKey.I)
+                        {
+                            Console.WriteLine("Deleting files... \n");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            ToDelete.ForEach(File.Delete);
+                            Console.WriteLine("Files deleted successfully. \n");
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        }
+                        Console.WriteLine("Press Q key to quit.\n");
+                    } while (cki.Key != ConsoleKey.Q);
 
 
 
-            }
-            else
-            {
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\nNo files to delete.");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("\nPress ENTER to exit.");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.ReadLine();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\nNo files to delete.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine("\nPress ENTER to exit.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.ReadLine();
+                }
             }
         }
     }
-}
